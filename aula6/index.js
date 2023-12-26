e1c734dfddf3337e1feea4dc8439ed5ebb8fd624
Vilson Lopes <vilsonlopes@yahoo.com.br>
2023-12-26 17:54:13 -0300

    Exercicios de treinamento

---
+++ aula6/index.js
@@ -0,0 +1,16 @@
+let nome = 'João';
+
+const nomeCompleto = 'Vilson Lopes';
+
+
+console.log(nome, 'nasceu em 1984');
+
+console.log('Meu nome é', nomeCompleto)
+
+// Operadores básicos do JS (+ - * /)
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+
+console.log(typeof (resultado))
---
+++ aula7/index.js
@@ -0,0 +1,12 @@
+
+let nome = 'Vilson';
+let sobrenome = 'Lopes';
+let idade = 39;
+let peso = 90;
+let altura = 1.78;
+const imc = peso / (altura * altura);
+const anoNascimento = 2023 - 39
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}.`);
+console.log(`${nome} nasceu em ${anoNascimento}.`)
