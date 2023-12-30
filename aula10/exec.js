70a15ec4b5a45d2faaa3e4f32891466390e912d4
Vilson Lopes <vilsonlopes@yahoo.com.br>
2023-12-30 17:58:39 -0300

    prompt

---
+++ aula10/exec.js
@@ -0,0 +1,12 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+let suspenso = varA
+
+varA = varB;
+varB = varC;
+varC = suspenso;
+
+
+console.log(varA, varB, varC);
---
+++ aula10/js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1);
+num2 = parseInt(num2);
+
+let resultado = num1 + num2;
+
+alert(`A soma dos valores é ${resultado}`);
